46d5d057b55026b51096f843c7bccce3b05076a0
Cristopher <email.cristopher@gmail.com>
2023-11-26 06:20:59 +0800

    add wifi functionality

--- wifi/wifi.h
+++ wifi/wifi.h
@@ -1,6 +1,15 @@
 #ifndef WIFI_H
 #define WIFI_H
+#include "esp_netif.h"
 
+
+void wifi_init(void);
+
+esp_netif_t *wifi_init_softap(void);
+
+esp_netif_t *wifi_init_sta(void);
 
 void setupWifi(void);
+
+
 #endif // WIFI_H
